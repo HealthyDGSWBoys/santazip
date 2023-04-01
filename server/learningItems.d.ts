bd78d026e456e4ef701faef399dc011a0da3c844
qfourca <qfourca3305@gmail.com>
2023-04-01 12:29:19 +0900

    feat(server) learningItems

---
+++ server/learningItems.d.ts
@@ -0,0 +1,13 @@
+interface learningItems {
+    sequential: Array<{
+        name: "dropout" | "dense",
+        units: number,
+        activation: "relu" | "softmax" | "sigmoid" | "linear"
+    }>
+    processer: Array<"distance" | "angle" | "distance2">
+    optimizer: "Adam" | "SGD" | "RMSprop"
+    loss: "SparseCategoricalCrossentropy"
+    metrics: Array<string>
+    epochs: number
+    batch: number
+}
