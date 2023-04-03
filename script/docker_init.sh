8904c2efb0ab9ebeae3b9ac4a8251ce1af7b357f
qfourca <qfourca3305@gmail.com>
2023-04-03 15:46:54 +0900

    docker gpustat

--- script/docker_init.sh
+++ script/docker_init.sh
@@ -1,6 +1,7 @@
 apt-get update && apt-get install -y libgl1-mesa-dev
 /usr/bin/python3 -m pip install --upgrade pip
 
+pip install gpustat
 pip install flask
 pip install flask-socketio
 pip install scikit-learn
