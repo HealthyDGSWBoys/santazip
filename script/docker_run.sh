15430d532016e0ef01405adc327e44efeb2d2e80
qfourca <qfourca3305@gmail.com>
2023-04-03 15:35:47 +0900

    docker port fowarding

--- script/docker_run.sh
+++ script/docker_run.sh
@@ -1,1 +1,1 @@
-docker run --name santazip -p 6001:60001 -p 6002:60002 -p 6003:60003 --gpus all -it -v $(pwd):/santazip --rm tensorflow/tensorflow:2.11.0-gpu
+docker run --name santazip -p 60001:60001 -p 60002:60002 -p 60003:60003 --gpus all -it -v $(pwd):/santazip --rm tensorflow/tensorflow:2.11.0-gpu
